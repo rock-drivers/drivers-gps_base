6e67175a76fae3e0d53b6cd9a985c3c2701c217a
Gabriel Arjones <arjones@arjones.com>
2016-10-27 09:50:16 -0300

    Refactor test suite

--- test/test_Driver.cpp
+++ test/test_Driver.cpp
@@ -1,10 +1,10 @@
 #include <boost/test/unit_test.hpp>
-#include <gps_base/Dummy.hpp>
+#include <gps_base/Driver.hpp>
 
 using namespace gps_base;
 
 BOOST_AUTO_TEST_CASE(it_should_not_crash_when_welcome_is_called)
 {
-    gps_base::DummyClass dummy;
-    dummy.welcome();
+    gps_base::Driver driver;
+    driver.welcome();
 }
